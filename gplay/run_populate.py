1572de22ad0c53b66b72495ec07a969b28b2cd9c
Nikel Arteta <nikelarteta@Senspark-Mini-04s-Mac-mini.local>
2016-08-15 17:24:12 +0700

    - Initial version.

---
+++ gplay/run_populate.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import os, os.path
+
+cmd = "../../../app-stores-toolkit/populate-v2.py metadata -platform android -prj-path . -sheet 1KLcJtK8W0Ru84piytass28DTe8o00nP6jN1h6Tsauyg -customized-metadata-path ../src/gplay/metadata"
+print cmd
+os.system(cmd)
